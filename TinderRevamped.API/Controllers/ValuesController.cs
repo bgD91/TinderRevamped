f48927a61a43637b2775545cf405feaecb5e9ec5
bgD91 <32795242+bgD91@users.noreply.github.com>
2020-02-15 23:15:54 +0200

    Initial Commit

---
+++ TinderRevamped.API/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TinderRevamped.API.Data;
+
+namespace TinderRevamped.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            if(value == null) 
+            {
+                return BadRequest();
+            }
+            else
+            {
+            return Ok(value);
+            }
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value){}
+    }
+}
---
+++ TinderRevamped.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace TinderRevamped.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
